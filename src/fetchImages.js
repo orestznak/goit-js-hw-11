f4ad51f86833510f67f7b3ac0d4764859e7552c0
orestznak <orestznak@gmail.com>
2022-06-29 16:11:28 +0300

    creatGallery

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+
 const BASE_URL = 'https://pixabay.com/api/';
 const MY_KEY = '28335848-011b3dc949dd802b31558b1f8';
 
 export const fetchImages = (imagesQuery) => {
-    return axios.get(`${BASE_URL}?key=${MY_KEY}&q=${imagesQuery}&image_type=photo&orientation=horizontal&safesearch=true`)}
+    return axios.get(`${BASE_URL}?key=${MY_KEY}&q=${imagesQuery}&image_type=photo&orientation=horizontal&safesearch=true`)
+                .then(response => {return response.data})
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-// import axios from "axios";
+
 import Notiflix from "notiflix";
 import simpleLightbox from "simplelightbox";
 import { fetchImages } from "./fetchImages";
 
+
+
 const searchForm = document.querySelector('#search-form');
 const imgGallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
-
-// const BASE_URL = 'https://pixabay.com/api/';
-// const MY_KEY = '28335848-011b3dc949dd802b31558b1f8';
-
-
 
 
 searchForm.addEventListener('submit', searchImg);
@@ -18,49 +15,37 @@ searchForm.addEventListener('submit', searchImg);
 function searchImg(evt) {
     evt.preventDefault();
 
-    const imagesQuery = searchForm.elements.searchQuery.value;
+    const query = searchForm.elements.searchQuery.value;
 
-    fetchImages(imagesQuery)
-    console.log();
-
-    // ${BaseAudioContext}?key=${MY_KEY}&q=${imagesQuery}
-
-
-//     axios.get(`${BASE_URL}?key=${MY_KEY}&q=${imagesQuery}&image_type=photo`)
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
+    fetchImages(query).then(resp => {
+                            imgGallery.innerHTML = createGallery(resp.hits)})
+                      .catch(() => Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again'));
+    
 
 
-    //fetchImges()
+};
+
+// const createGallery = (data) => {
+//   return data.map(image => 
+//       `<div class="photo-card">
+//       <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+//       <div class="info">
+//         <p class="info-item">
+//           <b>Likes:${image.likes}</b>
+//         </p>
+//         <p class="info-item">
+//           <b>Views:${image.views}</b>
+//         </p>
+//         <p class="info-item">
+//           <b>Comments: ${image.comments}</b>
+//         </p>
+//         <p class="info-item">
+//           <b>Downloads:${image.downloads}</b>
+//         </p>
+//       </div>
+//     </div> `
+//   ).join('');
+
+// }
 
 
-
-
-
-}
-
-
-
-{/* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div> */}
