be3ca21a448966e5c41b2c362e38d35662cade5a
orestznak <orestznak@gmail.com>
2022-07-02 08:00:55 +0300

    дз11

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 
 import './css/style.css'
+
 import Notiflix from "notiflix";
-//import simpleLightbox from "simplelightbox";
+
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css"
+
 import { createGallery } from "./js/createGallery";
 import ImgApiService from "./js/imgApiService";
 import LoadMoreBtn from './js/loadMoreBtn';
@@ -16,33 +20,43 @@ const loadMoreBtn = new LoadMoreBtn({
     selector: '.load-more',
     hidden: true,
 });
+
 const imgApiService = new ImgApiService();
 let countImages = 0;
 
 searchForm.addEventListener('submit', searchImg);
 loadMoreBtn.refs.btn.addEventListener('click', onLoadMore);
+// imgGallery.addEventListener('click', showModal);
+
+// const lightbox = ('.gallery a').SimpleLightbox({captions: true, captionSelector: 'img', captionsData: 'alt', captionDelay: 250});
+
 
 function searchImg(evt) {
   evt.preventDefault();
 
-  loadMoreBtn.show();
-  loadMoreBtn.disabled();
-
   clearImgGallery();
   imgApiService.query = evt.currentTarget.elements.searchQuery.value.trim();
-  
-  
-  
+    
   imgApiService.resetPage();
     
   imgApiService.fetchImages()
       .then(data => {
+
         if (data.total === 0 || imgApiService.query === ''){
           notiflixFailure();
+          loadMoreBtn.hide();
+
         } else {
+          loadMoreBtn.show();
+          loadMoreBtn.disabled();
+
           appendImg(data.hits);
+          countImages = data.hits.length;
+          
+          loadMoreBtn.enable();
+          Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
         };
-        loadMoreBtn.enable();
+        
       }
         )
       .catch(() => notiflixFailure())
@@ -52,15 +66,19 @@ function onLoadMore(evt) {
   evt.preventDefault();
 
   loadMoreBtn.disabled();
+
   imgApiService.fetchImages()
       .then(data => {
         appendImg(data.hits);
         loadMoreBtn.enable();
-        
         countImages += data.hits.length;
-
-        if(countImages === data.total){
-          return Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        console.log(countImages);
+        // lightbox.refresh();
+        
+        if(countImages > data.totalHits){
+          loadMoreBtn.hide();
+          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+          
         }
       })
 }
@@ -76,3 +94,11 @@ function clearImgGallery() {
 function notiflixFailure() {
   return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');
 }
+
+// function showModal(event) {
+//   if (event.target.nodeName !== 'IMG') {
+//       return;
+//   }
+
+//   event.preventDefault();
+// }
